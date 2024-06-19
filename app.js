6b927417938a41f01a07d645c6b1601ebe530482
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-06-19 01:21:01 +0000

    update

--- app.js
+++ app.js
@@ -1,43 +1,18 @@
-/*const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); // Allows the request to continue to the next middleware in line
-});
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log("In another middleware!");
-  //...
-  res.send("<h3>Hello from Express!</h3>");
-});
-
-const server = http.createServer(app);
-
-server.listen(3000);
-*/
-
-const express = require("express");
-
-const app = express();
-
-app.use("/", (req, res, next) => {
-  console.log("This always runs!");
-  next();
-});
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name"title"><button type="submit">Add</button></form>',
-  );
-});
-
-app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h3>Hello from Express!!!</h3>");
+  res.status(404).send("<h3>Page not found!!!</h3>");
 });
 
 app.listen(3000);
